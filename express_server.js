e4cd8256ec5ac83adff74ac12c2bd504bd546d08
kieranSharley <ksharley9@gmail.com>
2020-09-29 22:41:21 +0000

    added delete function

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const morgan = require('morgan');
 const app = express();
-const PORT = 8080;
+const PORT = 8080
+  ;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,7 +33,7 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   //let longURL = req.body.longURL;
-  console.log(req.body);
+  //console.log(req.body);
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
@@ -42,9 +44,13 @@ app.get("/u/:shortURL", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
-  const longURL = urlDatabase[req.params.shortURL];
-
-  res.redirect(longURL);
+});
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+  //  urlDatabase[shortURL] = req.body.longURL;
+  //when buttom is pressed browser runs this logic = <form method="POST" action="/urls/<%= url %>/delete"> 
 });
 
 app.listen(PORT, () => {
@@ -52,7 +58,7 @@ app.listen(PORT, () => {
 });
 
 function generateRandomString() {
-  return Math.random().toString(36).substr(1, 5);
+  return Math.random().toString(36).substr(2, 6);
 }
 
 //put them in the database short and long
