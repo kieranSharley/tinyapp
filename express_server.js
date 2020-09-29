0993d8590609cd7e7b1641829bb0b39824156d1a
kieranSharley <ksharley9@gmail.com>
2020-09-29 19:32:09 +0000

    nodemon installed d3d2

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080;
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -21,29 +21,50 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  //let longURL = req.body.longURL;
+  console.log(req.body);
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+  const longURL = urlDatabase[req.params.shortURL];
+
+  res.redirect(longURL);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-const afunc =function generateRandomString() {
- return Math.random().toString(36).substr(6);
-  
+function generateRandomString() {
+  return Math.random().toString(36).substr(1, 5);
 }
-console.log(afunc())
 
+//put them in the database short and long
+/*
+We first learned how to respond with a redirect after receiving a POST request. //yes
+We generated a new shortURL and then redirected the user to this new url.  //yes
+We learned that when the browser receives a redirection response,
+it does another GET request to the url in the response.    //ok~~~~~~~
+We created a new route for handling our redirect links;   // no~~~~
+this route obtained the shortURL from the route parameters,
+looked up the corresponding longURL from our urlDatabase,
+and responded with a redirect to the longURL.
+Finally, we tested that our new route is working as expected by making requests
+to it with the command line tool curl and our browser.
+*/
